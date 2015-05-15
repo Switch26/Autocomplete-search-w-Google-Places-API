9615643ef7228e879f39d63ce12d8bc068b532e9
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2015-05-15 14:33:55 -0400

    cleaned up

--- SearchBar/UISearchControllerVC.swift
+++ SearchBar/UISearchControllerVC.swift
@@ -22,17 +22,10 @@ class UISearchControllerVC: UIViewController, UITableViewDataSource, UISearchRes
     var searchController: UISearchController!
     var placesData = [String]()
     
-    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
-        "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
-        "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
-        "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
-        "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
-        filteredData = data
         
         // Initializing with searchResultsController set to nil means that
         // searchController will use this view controller to display the search results
